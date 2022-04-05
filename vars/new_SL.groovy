cd21da4fa8cad96573bc0e9d88a12ee296179d3c
Raja-gopalan <47553001+Raja-gopalan@users.noreply.github.com>
2022-04-05 17:14:28 +0530

    Update new_SL.groovy

--- vars/new_SL.groovy
+++ vars/new_SL.groovy
@@ -1,3 +1,6 @@
+import sonar-scan.groovy
+
+
 def call(Map params) {
   def last_deploy_id_file = params.last_deploy_id_file
   def chg_dir = "${params.chg_dir}"
@@ -42,6 +45,13 @@ def call(Map params) {
     echo latest_commit_URL
     echo '=================='
     
+    echo '=================='
+    echo 'starting sonar-scanner'
+    echo '=================='
+                           
+    sonar(chg_dir: '/opt/jenkins/workspace/RankMF-Stag', app_name: '${app_name}')
+                           
+                           
     //Email functions started here
     emailext body:"<h3 style=text-align: left;><img src=/home/rajagopalan/Downloads/jenkins2.png alt='' width=75 height=100 />Build Approval</h3><table border=1><tbody><tr><td>Project Name:</td><td>${JOB_NAME}</td></tr><tr><td>Environment:</td><td>&nbsp;</td></tr><tr><td>Committer Name:</td><td>${author_name}</td></tr><tr><td>Build URL:</td><td>${BUILD_URL}</td></tr><tr><td>Date of Build:</td><td>${date_of_build}</td></tr></tbody></table><p>&nbsp;</p><p>Please go to console and approve</p><p><a href=${BUILD_URL}input>${BUILD_URL}</a></p><h3>Changes</h3><p><a href=${latest_commit_URL}input>${latest_commit_URL} </a></p><p>&nbsp;</p><p>Regards,<strong>DevOps Team - Samco Securities</strong><strong><a href=mailto:devops@samco.in>devops@samco.in</a></strong> </br>Please be informed that this is an automated email generated from system. For any queries, kindly reach out to devops@samco.in.</p>",
       mimeType: 'text/html',
