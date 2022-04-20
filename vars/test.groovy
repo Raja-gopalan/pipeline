31e07306510dea1f473647c7549e60e3f9eebe27
Raja-gopalan <47553001+Raja-gopalan@users.noreply.github.com>
2022-04-20 10:52:17 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,7 +1,7 @@
 def call(Map params) {
 
   allfunctions.function1(name: 'Raja')
-  allfunctions.function1('Hello world via function2')
+  allfunctions.function2('Hello world via function2')
   
 }
 
